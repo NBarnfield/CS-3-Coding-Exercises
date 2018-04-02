b2634e8318bb491d5798aa8a3851c2892fd5e20e
NBarnfield <22405972+NBarnfield@users.noreply.github.com>
2018-04-02 11:16:35 +1000

    Delete sum.py

--- sum.py
+++
@@ -1,4 +0,0 @@
-print("Let's add two numbers together!")
-x = int(input("What is the first number you would like to add: "))
-y = int((input("What is the second number you would like to add: ")))
-print("The sum of {} and {} is {}.".format(x, y, (x + y)))
