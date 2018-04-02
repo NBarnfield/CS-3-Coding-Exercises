fad47de73ac2c0aa42d50ea54031f6888d987cc4
NBarnfield <22405972+NBarnfield@users.noreply.github.com>
2018-04-02 11:53:53 +1000

    Int() changed to float() to accept decimal places, and & changed to and.

--- Chapter_2/Chapter2_ex5.py
+++ Chapter_2/Chapter2_ex5.py
@@ -1,8 +1,8 @@
-celcius_temperature = int(input("What is the temperature in degrees celcius at the moment? "))
+celcius_temperature = float(input("What is the temperature in degrees celcius at the moment? "))
 farenheit_temperature = (celcius_temperature) * 1.8 + 32
 if celcius_temperature <= 0:
     print("Wow, that is cold! {} degrees farenheit - you should grab a jacket!".format(farenheit_temperature))
-elif (celcius_temperature > 0) & (celcius_temperature < 30):
+elif (celcius_temperature > 0) and (celcius_temperature < 30):
     print("{} farenheit...acceptable weather!".format(farenheit_temperature))
 else:
     print("{} farenheit!?!? Madness! Get into the shade!!".format(farenheit_temperature))
