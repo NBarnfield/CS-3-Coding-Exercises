b39767988bf3b43610a97b9c4eec52c8315474df
NBarnfield <22405972+NBarnfield@users.noreply.github.com>
2018-04-21 22:11:43 +1000

    Work out whether a function or an if statement works.

--- Checkpoint_1/parrot.py
+++ Checkpoint_1/parrot.py
@@ -6,46 +6,44 @@
 # Make sure all test cases below will pass.
 # parrot_trouble(talking, hour)
 
-def parrot_trouble(talking, hour):
-    #TODO: nested if statements for the hours? Seperate if statements?
-    if talking == True and hour < 7 or hour > 20:
+
+def current_hour(time):
+    """"Return a True/False computation of hour."""
+    if 7 > time < 20:
+        return True
+    elif 7 < time > 20:
+        return False
+    else:
+        print("That is not a valid answer.")
+        exit()
+
+
+def parrot_talking(noise):
+    """Request user input to determine if the parrot is speaking or not. Return a True or False response."""
+    if noise == 'y':
+        return True
+    elif noise == 'n':
+        return False
+    else:
+        print("That is not a valid answer.")
+        exit()
+
+
+def parrot_trouble(noise, time):
+    """Take two variables and determine if we are in trouble or not!"""
+    if noise is True and time is True:
         print("We could be in for some drama!")
-    #elif talking == False and hour < 7 or hour > 20:
-    #    print("Quiet....perfect.")
+    elif noise is True and time is False:
+        print("Who knew that this would happen...")
+    elif noise is False and time is False:
+        print("We're all clear!!")
     else:
         print("Quiet...perfect.")
 
-def current_hour():
-    "Set loop to request user input for the current hour. If the response is not within the 24 hour range then loop."
-    hour_counter = 0
-    while hour_counter == 0:
-        hour = float(input("What is the hour in 24 hour time (0-23)? "))
-        if 0 < hour > 23:
-            return hour
-        else:
-            print("That is not a valid response. Please enter an integer between 0 - 23.")
-
-def parrot_talking(talking):
-    "Request user input to determine if the parrot is speaking or not. Return a True or False response."
-    talking_counter = 0
-    while talking_counter == 0:
-        is_talking = input("Is the parrot talking (y/n)? ")
-        if is_talking == 'y':
-            talking = True
-            return talking
-        elif is_talking == 'n':
-            talking = False
-            return talking
-        else:
-            print("That is not a valid response, please enter y or n.")
-
-# Set variables
-hour = 0
-talking = 0
 
 # Ask user for the current hour
 try:
-    hour =
+    hour = float(input("What is the hour in 24 hour time (0-23)? "))
     current_hour(hour)
 except:
     print("There appears to be an error with the current_hour function.")
@@ -53,6 +51,7 @@ except:
 
 # Ask user if the parrot is talking
 try:
+    talking = input("Is the parrot talking (y/n)? ")
     parrot_talking(talking)
 except:
     print("There appears to be an error with the parrot_talking function.")
@@ -64,3 +63,4 @@ try:
 except:
     print("There appears to be an error with the parrot_trouble function.")
     exit()
+
