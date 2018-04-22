59ca209dfdfd8166f4bc099c42dc8ef63f4aadfd
NBarnfield <22405972+NBarnfield@users.noreply.github.com>
2018-04-22 21:51:55 +1000

    Example program from Python for Everybody.

---
+++ Python_for_everybody_examples/repeated_steps.py
@@ -0,0 +1,9 @@
+# An introduction to loops and iteration variables
+
+n = 5
+while n > 0:
+    print(n)
+    # Reduce n by one - iterate over it - and when the while statement is no longer True and so completes the loop.
+    n = n - 1
+print("Blastoff!")
+print(n)
