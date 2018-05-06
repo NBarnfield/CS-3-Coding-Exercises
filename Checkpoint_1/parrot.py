81e530efab37765f00b6626e5a8a86e4f669e631
NBarnfield <22405972+NBarnfield@users.noreply.github.com>
2018-05-06 11:26:03 +1000

    Checkpoint 1 exercises

--- Checkpoint_1/parrot.py
+++ Checkpoint_1/parrot.py
@@ -1,66 +1,56 @@
-# Program name: parrot_trouble.py
-# We have a loud talking parrot. The "hour" input is the current hour time
-# in the range 0..23. We are in trouble if the parrot is talking
-#(input True if talking and False if not) and the hour is before 7 or after 20.
-#Return True if we are in trouble.
-# Make sure all test cases below will pass.
+# We have a loud talking parrot.
+# The "hour" input is the current hour time in the range 0..23.
+# We are in trouble if the parrot is talking(input True if talking and False if not) and the hour is before 7 or after 20.
+# Return True if we are in trouble.
 # parrot_trouble(talking, hour)
 
 
-def current_hour(time):
-    """"Return a True/False computation of hour."""
-    if 7 > time < 20:
+def parrot_talking(talking, hour):
+    if talking is True and hour not in range(7, 20):
         return True
-    elif 7 < time > 20:
+    else:
         return False
-    else:
-        print("That is not a valid answer.")
-        exit()
 
 
-def parrot_talking(noise):
-    """Request user input to determine if the parrot is speaking or not. Return a True or False response."""
-    if noise == 'y':
-        return True
-    elif noise == 'n':
-        return False
-    else:
-        print("That is not a valid answer.")
-        exit()
+# Get user input on what the hour is in 24 hour time.
+while True:
+    try:
+        current_time = int(input("What is the current hour (0 - 23)? "))
+        if current_time in range(0, 23):
+            break
+        else:
+            print("That is not a valid answer, please enter an integer or float between 0 - 23.")
+
+    except ValueError:
+        print("That is not a valid answer, please enter an integer or float between 0 - 23.")
 
 
-def parrot_trouble(noise, time):
-    """Take two variables and determine if we are in trouble or not!"""
-    if noise is True and time is True:
-        print("We could be in for some drama!")
-    elif noise is True and time is False:
-        print("Who knew that this would happen...")
-    elif noise is False and time is False:
-        print("We're all clear!!")
-    else:
-        print("Quiet...perfect.")
+# Get user input on whether or not the parrot is talking
+while True:
+    try:
+        current_talking = input("Is the parrot talking (Y/N)? ")
+        if current_talking == "Y":
+            current_talking = True
+            break
+
+        elif current_talking == "N":
+            current_talking = False
+            break
+        else:
+            print("That is not a valid answer, please enter the character Y or N.")
+
+    except ValueError:
+        print("That is not a valid answer, please enter the character Y or N.")
 
 
-# Ask user for the current hour
-try:
-    hour = float(input("What is the hour in 24 hour time (0-23)? "))
-    current_hour(hour)
-except:
-    print("There appears to be an error with the current_hour function.")
-    exit()
+# Confirm the values that were returned as a result of the user's input
+print("So the time is {}, and the parrot is talking {}...".format(current_time, current_talking))
 
-# Ask user if the parrot is talking
-try:
-    talking = input("Is the parrot talking (y/n)? ")
-    parrot_talking(talking)
-except:
-    print("There appears to be an error with the parrot_talking function.")
-    exit()
+# Pass those values in as arguments to our user defined function
+trouble = parrot_talking(current_talking, current_time)
 
-# Compute the result of our scenario
-try:
-    parrot_trouble(talking, hour)
-except:
-    print("There appears to be an error with the parrot_trouble function.")
-    exit()
-
+# Based on the results of our function determine if we are in trouble!
+if trouble is True:
+    print("You better get out of there, it's going to get messy real soon!!")
+else:
+    print("Awesome, you can cruise on by!")
