c8dd0f629cdb269f342e9f4648852b2beb131418
NBarnfield <22405972+NBarnfield@users.noreply.github.com>
2018-05-21 11:09:39 +1000

    Chapter 7 exercises

--- Chapter_8/Exercise_1.py
+++ Chapter_8/Exercise_1.py
@@ -1,6 +1,32 @@
-Exercise 1:
+# Exercise 1:
+#
+# Write a function called chop that takes a list and modifies it, removing the first and last elements, and returns None.
+#
+# Then write a function called middle that takes a list and returns a new list that contains all but the first and last elements.
 
-Write a function called chop that takes a list and modifies it, removing the first and last elements, and returns None.
 
-Then write a function called middle that takes a list and returns a new list that contains all but the first and last elements.
+def chop(list):
+    list.pop()
+    list.remove[0]
+    return None
+
+
+def middle(truncated_list):
+
+
+
+
+# Create a list
+num_list = list()
+
+# Create a while loop to accept floats and integers and store them in a list. If invalid data is entered use except.
+while True:
+    try:
+        inp = input("Please enter an integer or float: ")
+        if inp == 'done': break
+        value = float(inp)
+        numlist.append(value)
+
+    except ValueError:
+        print("That is not a valid entry - please enter an integer or float.")
 
