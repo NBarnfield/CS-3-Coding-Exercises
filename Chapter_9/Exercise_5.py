8ad9e93292d92a3e77e3d87628b894aa614b4e28
NBarnfield <22405972+NBarnfield@users.noreply.github.com>
2018-07-02 07:25:01 +1000

    Update for Ex 5.

---
+++ Chapter_9/Exercise_5.py
@@ -0,0 +1,31 @@
+# Ask user for file name input.
+file = input("Enter file: ")
+
+# Attempt to open the file, otherwise quit on failure.
+try:
+    fhand = open(file, 'r')
+except ValueError:
+    print("File name {} is not valid.".format(file))
+    quit()
+
+# Construct a dictionary
+mbox_text = dict()
+
+# Calculate the frequency of days the emails were sent
+try:
+    for line in fhand:
+        if not line.startswith('From '): continue
+        line = line.translate(line.maketrans('', '', '@'))
+        line = line.rstrip().split()
+        word = line[2]
+        mbox_text[word] = mbox_text.get(word, 0) + 1
+
+except ValueError:
+    print("There appears to be an error with {}. Please try again.".format(fhand))
+    quit()
+
+# Print results
+if mbox_text == {}:
+    print("Whoops, no luck! There were no lines that started with 'From '!!")
+else:
+    print(mbox_text)
