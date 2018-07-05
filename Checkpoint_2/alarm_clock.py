7030f9814f5628e6c1a0a6566f92ff624e78f0a0
NBarnfield <22405972+NBarnfield@users.noreply.github.com>
2018-07-06 08:36:43 +1000

    Update with function for Checkpoint 2.

--- Checkpoint_2/alarm_clock.py
+++ Checkpoint_2/alarm_clock.py
@@ -4,67 +4,33 @@
 # are on vacation -- then on weekdays it should be "10:00" and weekends it should be "off".
 # Make sure all test cases below will pass.
 # Define function - alarm_clock(day, vacation)
-#
-#
-# def alarm_clock(day, vacation):
-#     """Set the alarm for tomorrow based on what day it will be and whether or not you are on vacation."""
-#     try:
-#     # Weekdays
-#         if day in range(1, 6):
-#             if vacation == 'True':
-#                 print("You can get up tomorrow at 10:00")
-#             else:
-#                 print("You can get up at 7:00 tomorrow")
-#
-#     # Weekends
-#         if day == 0 or day == 6:
-#             if vacation == 'True':
-#                 print("You don't need an alarm! Sleep in as long as you'd like.")
-#             else:
-#                 print("You are not on vacation but it is a weekend - your alarm is set for 7:00")
-#
-#     except ValueError:
-#         print("Invalid arguments given...don't go to sleep - that way you don't need an alarm!")
-#
-#
-# # Collect user input as to what day it will be tomorrow.
-# while True:
-#     try:
-#         week = {"Sunday": 0, "Monday": 1, "Tuesday": 2, "Wednesday": 3, "Thursday": 4, "Friday": 5, "Saturday": 6}
-#         tomorrow = int(input("What day is it tomorrow 0-6 where {} ? ".format(week)))
-#
-#         if tomorrow in range(0, 7):
-#             break
-#
-#         else:
-#             print("That is not a valid answer. Please enter an integer between 0-6")
-#
-#     except ValueError:
-#         print("That is not a valid argument. Please enter an integer between 0-6.")
-#
-#
-# # Collect user input as to whether they are on vacation or not.
-# while True:
-#     try:
-#         holiday = input("Are you on vacation (True/False)? ")
-#         if holiday == 'True':
-#             break
-#
-#         elif holiday == "False":
-#             break
-#
-#         else:
-#             print("That is not a valid answer. Please enter True or False.")
-#
-#     except ValueError:
-#         print("That is not a valid argument. Please enter the string True or False.")
+
+def alarm_clock(day, vacation):
+    """Set the alarm for tomorrow based on what day it will be and whether or not you are on vacation."""
+    try:
+    # Weekdays
+        if day in range(1, 6):
+            if vacation is True:
+                print("You can get up tomorrow at 10:00")
+            else:
+                print("You can get up at 7:00 tomorrow")
+
+    # Weekends
+        if day == 0 or day == 6:
+            if vacation is True:
+                print("You don't need an alarm! Sleep in as long as you'd like.")
+            else:
+                print("You are not on vacation but it is a weekend - your alarm is set for 7:00")
+
+    except ValueError:
+        print("Invalid arguments given...don't go to sleep - that way you don't need an alarm!")
 
 
 def true_false(question):
     while True:
         try:
             answer = input(question)
-            answer.lower()
+            answer = answer.lower()
             if answer == 'true':
                 return True
             elif answer == 'false':
@@ -75,13 +41,23 @@ def true_false(question):
             print("That is not a valid argument. Please enter the string True or False.")
 
 
-vacation = true_false("Are you on holiday?")
+def what_day(day_question):
+    while True:
+        try:
+            day = int(input(day_question))
+            if day in range(0, 7):
+                return day
+            else:
+                print("That is not a valid answer. Please enter an integer between 0-6")
+
+        except ValueError:
+            print("That is not a valid argument. Please enter an integer between 0-6.")
 
 
-if vacation is True:
-    print("Yay, you're on vacation")
-else:
-    print("Oh no, that's a shame!")
+# Run the code
+week = {"Sunday": 0, "Monday": 1, "Tuesday": 2, "Wednesday": 3, "Thursday": 4, "Friday": 5, "Saturday": 6}
+holiday = true_false("Are you on holiday? (True/False)")
+tomorrow = what_day("What day is it tomorrow 0-6 where {} ? ".format(week))
 
 # Pass in day and vacation in as arguments to see what time the user should get up tomorrow.
-# alarm_clock(tomorrow, holiday)
+alarm_clock(tomorrow, holiday)
