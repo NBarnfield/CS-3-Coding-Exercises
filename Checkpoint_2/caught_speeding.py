e67384709d5d7481a391766cb69618edef44fdfe
NBarnfield <22405972+NBarnfield@users.noreply.github.com>
2018-07-06 12:50:50 +1000

    Update with function for Checkpoint 2.

--- Checkpoint_2/caught_speeding.py
+++ Checkpoint_2/caught_speeding.py
@@ -1,17 +1,41 @@
-# Program name: caught_speeding.py
-# You are driving a little too fast, and a police officer stops you.
-# Write code to compute the result, encoded as an int value: 0=no ticket, 1=small ticket, 2=big ticket.
-# If speed is 60 or less, the result is 0. If speed is between 61 and 80 inclusive, the result is 1.
-# If speed is 81 or more, the result is 2.
-# Unless it is your birthday -- on that day, your speed can be 5 higher in all cases.
-# Make sure all test cases below will pass.
-# caught_speeding(speed, is_birthday):
+def today_birthday(question):
+    while True:
+        try:
+            user_birthday = input(question)
+            if user_birthday in ("true", "True"):
+                print("Congratulations! Maybe I didn't see you go so quickly there...")
+                return True
+
+            elif user_birthday in ("false", "False"):
+                print("Well, just another day for you then is it...no special treatment then.")
+                return False
+
+            else:
+                print("That is not a valid response. Please enter True or False.")
+
+        except ValueError:
+            pass
+
+
+def what_speed(speed_question):
+    while True:
+        try:
+            speed_confession = float(input(speed_question))
+            if speed_confession > 0:
+                print("So you were going {}km an hour...".format(speed_confession))
+                return speed_confession
+
+            else:
+                print("That isn't what I saw. Please repeat how fast you were going as an integer or float, buddy.")
+
+        except ValueError:
+            print("That is not a valid answer. Please enter an integer or float.")
 
 
 def caught_speeding(speed, is_birthday):
 
     # If it is your birthday then reduce the speed by 5.
-    if is_birthday == 'True':
+    if is_birthday is True:
         speed = speed - 5
         print("Considering it is your birthday, let's say you were doing {}.".format(speed))
     else:
@@ -33,70 +57,21 @@ def caught_speeding(speed, is_birthday):
 
     else:
         print("Valid input was not passed in as arguments. Go to jail, do not pass go and collect $200.")
+    print("That is not a valid response. Please enter True or False.")
 
 
-# Initialise variables
-ticket = None
-birthday = None
-current_speed = None
+def fine(ticket_value):
+    if ticket_value == 0:
+        print("No fine for you today, Sir. Have a 'fine' day.")
+
+    elif ticket_value == 1:
+        print("You deserve a small ticket for your crimes to public safety.")
+
+    else:
+        print("I don't know if I should be perturbed or inspired. Here's a massive fine for being a maniac.")
 
 
-# Ask the user if it is their birthday.
-
-def today_birthday(question)
-    while True:
-        try:
-            user_birthday = input(question)
-            user_birthday = user_birthday.lower()
-
-
-
-
-
-while True:
-    try:
-
-        if birthday == "True":
-            print("Congratulations! Maybe I didn't see you go so quickly there...")
-            break
-
-        elif birthday == "False":
-            print("Well, just another day for you then is it...no special treatment then.")
-            break
-
-        else:
-            print("That is not a valid response. Please enter True or False.")
-
-    except ValueError:
-        print("That is not a valid response. Please enter True or False.")
-
-
-# Ask user for input to tell us what speed they were doing.
-while True:
-    try:
-        current_speed = float(input("What speed do you think you were doing there? "))
-        if current_speed > 0:
-            print("So you were going {}km an hour...".format(current_speed))
-            break
-
-        else:
-            print("That isn't what I saw. Please repeat how fast you were going as an integer or float, buddy.")
-
-    except ValueError:
-        print("That is not a valid answer. Please enter an integer or float.")
-
-
-# Given this information, calculate whether to give them a fine or not.
+birthday = today_birthday("Is it your birthday today (True/False)? ")
+current_speed = what_speed("What speed do you think you were doing there? ")
 ticket = caught_speeding(current_speed, birthday)
-
-if ticket == 0:
-    print("No fine for you today, Sir. Have a 'fine' day.")
-
-elif ticket == 1:
-    print("You deserve a small ticket for your crimes to public safety.")
-
-else:
-    print("I don't know if I should be perturbed or inspired. Here's a massive fine for being a maniac.")
-
-
-birthday = is_birthday("Is it your birthday today (True/False)? ")
+fine(ticket)
