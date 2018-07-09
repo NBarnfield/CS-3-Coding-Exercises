b1d99fafe15421980c995ec02c90a63c9a7d23b4
NBarnfield <22405972+NBarnfield@users.noreply.github.com>
2018-07-09 21:41:53 +1000

    Update checkpoint 1 exercises with functions.

--- Checkpoint_2/caught_speeding.py
+++ Checkpoint_2/caught_speeding.py
@@ -4,11 +4,9 @@ def today_birthday():
         if user_birthday in ("true", "True"):
             print("Congratulations! Maybe I didn't see you go so quickly there...")
             return True
-
         elif user_birthday in ("false", "False"):
             print("Well, just another day for you then is it...no special treatment then.")
             return False
-
         else:
             print("That is not a valid response. Please enter True or False.")
 
@@ -19,7 +17,6 @@ def what_speed():
         if speed_confession > 0:
             print("So you were going {}km an hour...".format(speed_confession))
             return speed_confession
-
         else:
             print("That isn't what I saw. Please repeat how fast you were going as an integer or float, buddy.")
 
@@ -37,15 +34,12 @@ def caught_speeding(speed, is_birthday):
         print("Hmmm that's a respectable pace.")
         print("You don't receive a ticket")
         return 0
-
     elif 61 <= speed <= 80:
         print("You were going a little quick there, Stan.")
         return 1
-
     elif speed >= 81:
         print("That's too fast!! You could have hurt someone! ")
         return 2
-
     else:
         print("Valid input was not passed in as arguments. Go to jail, do not pass go and collect $200.")
     print("That is not a valid response. Please enter True or False.")
@@ -54,10 +48,8 @@ def caught_speeding(speed, is_birthday):
 def fine(ticket_value):
     if ticket_value == 0:
         print("No fine for you today, Sir. Have a 'fine' day.")
-
     elif ticket_value == 1:
         print("You deserve a small ticket for your crimes to public safety.")
-
     else:
         print("I don't know if I should be perturbed or inspired. Here's a massive fine for being a maniac.")
 
