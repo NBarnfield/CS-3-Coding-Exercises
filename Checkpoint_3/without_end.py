04c59833550f23e4aeb01086c0342b0966a9199d
NBarnfield <22405972+NBarnfield@users.noreply.github.com>
2018-07-11 17:31:11 +1000

    Update checkpoint 1 exercises with functions.

--- Checkpoint_3/without_end.py
+++ Checkpoint_3/without_end.py
@@ -2,12 +2,13 @@ def ask_for_string():
     while True:
         user_input = input("Please enter an awesome alphabetic string with at least 2 characters: ")
         if len(user_input) > 2 and isinstance(user_input, str):
-            return user_input
+            altered_input = user_input[1:-1]
+            return user_input, altered_input
         else:
             print("That is not valid input. Please enter an awesome alphabetic string.")
 
 
-string = ask_for_string()
-new_string = string[1:-1]
-print("This is the string you wrote {} and the middle of the word is {}".format(string, new_string))
+# Store multiple return values into variables and print
+string, altered_string = ask_for_string()
+print("This is the string you wrote {} and the middle of the word is {}".format(string, altered_string))
 
