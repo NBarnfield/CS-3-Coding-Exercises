e575c75d9c4975863b31f5dac2a89cbd4ad878af
NBarnfield <22405972+NBarnfield@users.noreply.github.com>
2018-07-13 16:10:04 +1000

    Checkpoint 3 exercises

---
+++ Checkpoint_3/aus_co_grants.py
@@ -0,0 +1,23 @@
+import csv
+
+
+def open_file():
+    """Open file"""
+    file = input("Enter file: ")
+    try:
+        fhand = open(file, 'r')
+        return fhand
+    except IOError:
+        print("{} is not a valid file.".format(file))
+        quit()
+
+
+ifile = open_file()
+reader = csv.reader(ifile)
+
+
+for line in reader:
+    if line[9] == 'AUSTRALIA' : continue
+    grant_state_dict = dict()
+    for state in line:
+        grant_state_dict[State] = grant_state_dict.get(state, 0) + 1
