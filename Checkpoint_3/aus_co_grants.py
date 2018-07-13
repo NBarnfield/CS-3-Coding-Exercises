59b30b2278508e50cf12ed0f96d092b492300667
NBarnfield <22405972+NBarnfield@users.noreply.github.com>
2018-07-14 09:17:20 +1000

    If Country is AU calculate the distribution of grants to each state.

--- Checkpoint_3/aus_co_grants.py
+++ Checkpoint_3/aus_co_grants.py
@@ -12,15 +12,23 @@ def open_file():
         quit()
 
 
+# Open and read file
 ifile = open_file()
 reader = csv.reader(ifile)
 
+# Create a dictionary to store results
+grant_state_dict = dict()
 
+# Loop through each line and calculate if the grant was given in Australia.
 for line in reader:
-    if line[9] == 'AUSTRALIA' : continue
-    grant_state_dict = dict()
-    for state in line:
-        state_of_grant = line[8]
-        grant_state_dict['State'] = grant_state_dict.get(state_of_grant, 0) + 1
+    # List comprehension is awesome!
+    line = [x.lower().rstrip() for x in line]
+    if line[9] == 'australia':
+        print(line)
+    # state_of_grant = line[8]
 
-print(grant_state_dict)
+#     grant_state_dict[state_of_grant] = grant_state_dict.get(state_of_grant, 0) + 1
+#
+#
+# for key in grant_state_dict:
+#     print(key, grant_state_dict[key])
