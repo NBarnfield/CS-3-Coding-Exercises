6d994b78f8bf127db18a4dbf7f2cd241a1eb25e5
NBarnfield <22405972+NBarnfield@users.noreply.github.com>
2018-07-15 15:41:40 +1000

    Quick count summary of Australia Council Grants

--- Checkpoint_3/aus_co_grants.py
+++ Checkpoint_3/aus_co_grants.py
@@ -15,44 +15,50 @@ def open_file():
 def sort_values(dictionary):
     t = list()
     new_t = list()
-    for key, value in dictionary.items():
-        t.append((value, key))
+    for k, v in dictionary.items():
+        t.append((v, k))
     t.sort(reverse=True)
-    for value, key in t:
-        new_t.append((key, value))
-        return new_t
-
-
-def largest_in_dict(dictionary, key):
-    most_common_value = None
-    most_common_key = None
-    for value in dictionary:
-        if most_common_value is None or dictionary[key] > most_common_value:
-            most_common_value = dictionary[key]
-            most_common_key = value
-    return most_common_value, most_common_key
+    for v, k in t:
+        new_t.append((k, v))
+    return new_t
 
 
 # Open and read file
 file = open_file()
 reader = csv.reader(file)
+next(reader)
 
 # Create a dictionary to store results
 grantState = dict()
 grantMoney = dict()
+grantYear = dict()
 
 # Loop through each line and calculate if the grant was given in Australia.
 for line in reader:
-    # List comprehension is awesome!
-    line = [x.lower().rstrip() for x in line]
-    if line[8] == 'australia':
+    if line[8] == 'AUSTRALIA':
         state_of_grant = line[7]
         grantState[state_of_grant] = grantState.get(state_of_grant, 0) + 1
         sum_of_grant = line[4]
         grantMoney[sum_of_grant] = grantMoney.get(sum_of_grant, 0) + 1
 
+        # Split the dates and take the year
+        year_of_grant = line[6]
+        year_of_grant = year_of_grant.split('-')
+        year_of_grant = year_of_grant[2]
+        grantYear[year_of_grant] = grantYear.get(year_of_grant, 0) + 1
 
-# Construct a list to sort the grants
-print("The number of grants given to each state over the last 10 years is:\n")
-print(sort_values(grantState))
-print("\nThe most common value grant distributed was {}".format(largest_in_dict(grantMoney, sum_of_grant)))
+
+print("The number of grants given to each state over the last 10 years is:")
+sorted_grant_state = sort_values(grantState)
+for key, value in sorted_grant_state:
+    print(key, value)
+
+print("\nThe top 10 most common value grants distributed were:")
+sorted_grant_money = sort_values(grantMoney)
+for key, value in sorted_grant_money[:10]:
+    print(key, value)
+
+print("\nThe year in which the most grants were given was:")
+sorted_year_of_grant = sort_values(grantYear)
+for key, value in sorted_year_of_grant:
+    print(key, value)
